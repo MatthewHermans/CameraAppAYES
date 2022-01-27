47b90f3183d155b75c9c3b255445aaefef945677
matthew <matthew.hermans@icloud.com>
2022-01-27 09:13:58 +0100

    Initial Commit

---
+++ CameraAppAYES/CameraAppAYESApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppAYESApp.swift
+//  CameraAppAYES
+//
+//  Created by matthew hermans on 27/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppAYESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
